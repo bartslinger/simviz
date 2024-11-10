11909bbe3b189b68730779f9e65513aa2fdb2563
Bart Slinger <bartslinger@gmail.com>
2024-11-10 14:43:22 +0800

    overflow scroll shadows with js

--- src/form.ts
+++ src/form.ts
@@ -20,3 +20,31 @@ $('#iyz').on('input', function () {
 		$('#izy').val(-value);
 	}
 });
+
+const updateScrollShadows = () => {
+	const div = $('#scroll-container');
+	const scrollTop = div.scrollTop() ?? 0.0;
+	const height = div.innerHeight() ?? 0.0;
+	const scrollHeight = div.prop('scrollHeight');
+
+	const scrollBottom = scrollHeight - height - scrollTop;
+
+	if (scrollTop <= 20) {
+		console.log('hide');
+		$('#scroll-shadow-top').css('opacity', 0.05 * scrollTop);
+	} else {
+		$('#scroll-shadow-top').css('opacity', 1);
+	}
+
+	if (scrollBottom <= 20) {
+		console.log('hide');
+		$('#scroll-shadow-bottom').css('opacity', 0.05 * scrollBottom);
+	} else {
+		$('#scroll-shadow-bottom').css('opacity', 1);
+	}
+};
+updateScrollShadows();
+
+$('#scroll-container').on('scroll', function () {
+	updateScrollShadows();
+});
